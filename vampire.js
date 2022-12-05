d9ffcbb0e853c1a00fcadfd6951908e50802fc43
lian-c <lian.chan@telus.com>
2022-12-04 17:14:51 -0800

    Update formatting

--- vampire.js
+++ vampire.js
@@ -10,22 +10,33 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberToOriginal = 0;
+    let currentVampire = this;
 
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberToOriginal++;
+    }
+    return numberToOriginal;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return true;
+    }
+    return false;
   }
 
   /** Stretch **/
@@ -36,7 +47,32 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    let currentVampire = this; //this is first vampire being compared
+    let secondVampire = vampire;
+    
+    if (currentVampire.creator === null) { //root
+      return currentVampire;
+    }
+    if (secondVampire.creator === null) {
+      return secondVampire;
+    }
 
+    if (currentVampire === secondVampire) { //same vampire
+      return vampire;
+    }
+    if (currentVampire.name === secondVampire.creator.name) {
+      return currentVampire;
+    }
+    if (secondVampire.name === currentVampire.creator.name) {
+      return secondVampire;
+    }
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+    }
+    while (secondVampire.creator) {
+      secondVampire = secondVampire.creator;
+    }
+    return currentVampire;
   }
 }
 
